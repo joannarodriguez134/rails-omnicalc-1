3c7eb4c71686d014a424e6cbf6c54d73d8ad7e65
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-06 22:36:36 +0000

    Started on square new

--- app/controllers/omnicalc_controller.rb
+++ app/controllers/omnicalc_controller.rb
@@ -1,8 +1,14 @@
 class OmnicalcController < ApplicationController
 
+  # only classes are camel cased !
+
   def calculators
     render({ :template => "calc_templates/home" })
   
+  end
+
+  def square_calc
+    render( { template: "calc_templates/square_new"})
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
   get("/", { :controller =>"omnicalc", :action =>"calculators" })
+
+  get("/square/new", { :controller => "omnicalc", :action => "square_calc"})
+
+  
   
 end
